a4232af30a517de0c1326821512091bfd933edd7
TJHL <league@WTS9.attlocal.net>
2016-09-21 16:58:35 -0700

    in the long run no matter what we say daniel is more awesome than jake

---
+++ src/day3/greeter.java
@@ -0,0 +1,10 @@
+package day3;
+
+import javax.swing.JOptionPane;
+
+public class greeter {public static void main(String[] args) {
+	JOptionPane.showMessageDialog(null, "");
+    System.out.println("jakes var = poo ");
+}
+
+}
---
+++ src/day3/snooper.java
@@ -0,0 +1,20 @@
+package day3;
+
+import javax.swing.JOptionPane;
+
+import sun.tools.jar.resources.jar;
+
+public class snooper {public static void main(String[] args) {
+	String answer = JOptionPane.showInputDialog(" who is more awesome jake or daniel");
+	JOptionPane.showMessageDialog(null, answer);
+	
+	
+	
+	
+	
+	
+			
+				
+}
+
+}
